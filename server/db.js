7b4f7ac85eb296ad220d3690e73b5ce438750d1c
anshika <anshikasahu@gmail.com>
2026-04-07 08:41:29 +0530

    Added  full project

---
+++ server/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://test:test123@cluster0.u5dssrw.mongodb.net/batchDB?retryWrites=true&w=majority")
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log("ERROR:", err));
+
+module.exports = mongoose;
